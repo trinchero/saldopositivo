47cbdcf46de548117e2b272cf6ca8ae04a73ab9e
Andrea <andreatrinchero92@gmail.com>
2025-07-31 19:35:49 +0200

    update site.config

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,2 @@
-export const siteUrl = "https://mizar.majestico.co";
-export const siteTitle = "Mizar";
-export const defaultLocale = "en";
-export const locales = ["en", "es"];
-export type Locale = "en" | "it";
+export const siteUrl = "https://saldopositivo.vercel.app";
+export const siteTitle = "Saldo Positivo";
